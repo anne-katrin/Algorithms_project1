5fe5ea3c8f3dc09fddab8eaf49597c94ffb6107b
Linda Nilsson <tuss@linux.se>
2010-09-08 11:29:34 +0200

    generating POWERsets

--- Exercise1/Knapsack.java
+++ Exercise1/Knapsack.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.Character.Subset;
 import java.util.*;
 
 
@@ -9,12 +10,14 @@ public class Knapsack {
 	private String fulltext = null;
 	private int capacity = 0;
 	private ArrayList<Tuple> list; 
+	private ArrayList<ArrayList<Tuple>> subSets;
 	
 	/**
 	 * Constructor
 	 */
 	public Knapsack(){
 		list = new ArrayList<Tuple>();
+		subSets = new ArrayList<ArrayList<Tuple>>();
 	}
 	
 	/**
@@ -25,7 +28,50 @@ public class Knapsack {
 	public static void main(String[] args) throws IOException {
 		Knapsack knap = new Knapsack();
 		knap.readingTheFile();
-		String fulltext = null;
+		knap.createPowerset();
+		knap.printSubsets();
+	}
+	
+	public void createPowerset(){
+		int elements = list.size();
+		long noSubSets = (long) Math.pow(2, elements);
+		
+		for(long i = 0; i < noSubSets; i++){
+			long elem = 1;
+			ArrayList<Tuple> tempList = new ArrayList<Tuple>(); 
+			
+			for(int j = 0; j < elements; j++){
+				long b = i & elem;
+//				System.out.println("elem = " + elem);
+//				System.out.println("i = " + i);
+//				System.out.println("i & elem " + b);
+				if((i & elem) != 0){
+					tempList.add(list.get(j));
+					System.out.println("loop add " + list.get(j));
+					
+				}
+				
+				elem = elem << 1;
+				
+			}
+			subSets.add(tempList);
+		}
+	}
+	
+	public void printSubsets(){
+		Iterator itr = subSets.iterator(); 
+		int count = 0;
+		while(itr.hasNext()) {
+			System.out.println("set " + count);
+			count++;
+			ArrayList temp = (ArrayList) itr.next();
+			Iterator itTemp = temp.iterator();
+			while(itTemp.hasNext()){
+				Tuple hej = (Tuple) itTemp.next();
+				System.out.println("\t" + hej.getSize() + " " + hej.getValue());
+			}
+		}
+
 	}
 	
 	/**
@@ -35,12 +81,8 @@ public class Knapsack {
 	public void readingTheFile() throws IOException{
 		FileReader file = new FileReader("firstexample.txt");
 		BufferedReader reading = new BufferedReader(file); 
-		
-	
 		fulltext = new String();
-		char[] current;
-	current = new char[100];
-		char f;
+
 		try{
 			while((fulltext = reading.readLine()) != null){
 				if(fulltext.startsWith("capacity")){
@@ -57,7 +99,6 @@ public class Knapsack {
 				}
 				else{
 				String t = removePar(fulltext);
-				
 				StringTokenizer tock = new StringTokenizer(t, ",");
 				int size = 0;
 				int value = 0;
